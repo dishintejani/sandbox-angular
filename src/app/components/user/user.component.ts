0e26f13d2d8e07158b7fa94ad7319e410e024409
dishintejani <de.dishintejani@gmail.com>
2018-06-24 13:12:18 -0400

    add app/component to the file

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from './Models/User';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  // properties
+  user: User;
+
+  // methods
+  constructor() {
+    this.user = {
+      firstName : 'Joe',
+      lastName : 'Roe',
+      age : 30,
+      address : {
+        street: '370 Hollyberry Trail',
+        city: 'North York',
+        state: 'Ontario'
+      }
+    };
+  }
+
+  ngOnInit() {
+  }
+
+}
+
